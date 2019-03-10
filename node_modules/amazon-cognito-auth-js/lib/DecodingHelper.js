4ccc4582efd228372bad42dc3850bcdd5518ee1e
jordanjpark <jordanjpark@gmail.com>
2019-03-10 15:15:40 -0400

    changed Cognito config

--- node_modules/amazon-cognito-auth-js/lib/DecodingHelper.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-exports.__esModule = true;
-var decode = exports.decode = function decode(str) {
-  return global.atob(str);
-};
--- node_modules/amazon-cognito-auth-js/lib/UriHelper.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-exports.__esModule = true;
-var SELF = '_self';
-
-var launchUri = exports.launchUri = function launchUri(url) {
-  return window.open(url, SELF);
-};
